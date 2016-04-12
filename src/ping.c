9dbff90c95800d4d956c63d19a459d8c7e8a331e
Nicholas Horton <nhort031@uottawa.ca>
2016-04-12 12:44:59 -0400

    Added timeout for listen method

--- src/ping.c
+++ src/ping.c
@@ -16,8 +16,8 @@ void initPing() {
 int ping() {
 	DDRA |= BIT0; //Ensure ouput mode
 	PORTA &= ~BIT0; //Set ping to low
-	pulse_out(10);
-	return pulse_in();
+	pulse_out(10); //chirp for 10 microseconds
+	return pulse_in(10000); //time the echo delay
 }
 
 //speedSound determined based on ambient temp (v = 331m/s + 0.6m/s/C * T`)
@@ -27,24 +27,33 @@ double getDistance(double speedSound) {
 }
 
 //send a pulse (high value) for a given amount of time
-void pulse_out(int duration) {
+void pulse_out(unsigned long duration) {
 	PORTA |= BIT0; //high
-
+	double tStart = time_in_microseconds();
+	while (time_in_microseconds() - tStart <= duration);
 	PORTA &= ~BIT0; //low
 }
 
 //reads how long the pin reads the given value
 
-int pulse_in(){
+int pulse_in(unsigned long maxDelay){
 
 	DDRA &= ~BIT0; //Set to input mode
+	unsigned long tStart = time_in_microseconds();
 
 	/* Wait for pulse begin */
-	while (PORTA << 0 != 1)
-		;
-	unsigned long tStart = time_in_microseconds();
+	while (PORTA << 0 != 1){
+		if (time_in_microseconds() - tStart >= maxDelay){
+			return -1; //Error, no echo!
+		}
+	}
+
 	/* Wait for pulse end */
-	while (PORTA << 0 == 1)
-		;
+	// while (PORTA << 0 == 1){
+	// 	if (time_in_microseconds() - tStart >= maxDelay){
+	// 		return -1; //Error, no echo!
+	// 	}
+	// }
+
 	return time_in_microseconds() - tStart;
 }
